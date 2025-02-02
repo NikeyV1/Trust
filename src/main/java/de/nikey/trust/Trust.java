0bc3a2fd40d3ed514a50de5c45aa6203af156453
nikeyv1 <n.heidorn@outlook.de>
2025-02-02 20:51:12 +0100

    Initial commit

---
+++ src/main/java/de/nikey/trust/Trust.java
@@ -0,0 +1,17 @@
+package de.nikey.trust;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Trust extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
